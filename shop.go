1349038b9b20cd32045e11127b084139db980034
Marko Mikulic <mikulic.marko996@gmail.com>
2021-09-08 22:20:35 +0200

    Added body to Update method of ShopItem struct

--- shop.go
+++ shop.go
@@ -17,6 +17,7 @@ type ShopItem struct {
 	Shippable          bool       `gorm:"not null;default:false;" json:"shippable"`
 	Quantity           int        `gorm:"not null; default:0;" json:"quantity"`
 	StripeProductApiID string     `gorm:"not null; type:varchar(255)" json:"stripe_product_api_id"`
+	StripePriceApiID   string     `gorm:"type:varchar(255);" json:"stripe_price_api_id"`
 	CreatedAt          time.Time  `gorm:"not null;" json:"created_at"`
 	UpdatedAt          time.Time  `gorm:"not null;" json:"updated_at"`
 	DeletedAt          *time.Time `json:"-"`
@@ -66,16 +67,17 @@ func (i *ShopItem) Create(data *ShopItemCreate) error {
 		itemPrice = *data.ItemSalePrice
 	}
 
-	if _, err := data.createStripeProductPrice(stripeProduct, itemPrice); err != nil {
+	productPrice, err := data.createStripeProductPrice(stripeProduct, itemPrice)
+	if err != nil {
 		log.Printf("error occurred while creating stripe product price: %v", err)
 		return err
 	}
 
 	insertQuery := `INSERT INTO shop_items (item_name, item_picture, item_price, item_sale_price, item_description, 
-		shippable, quantity, stripe_product_api_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
+		shippable, quantity, stripe_product_api_id, stripe_price_api_id, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
 
 	params := []interface{}{i.ItemName, i.ItemPicture, i.ItemPrice, i.ItemSalePrice, i.ItemDescription, i.Shippable,
-		i.Quantity, stripeProduct.ID, i.CreatedAt, i.UpdatedAt}
+		i.Quantity, stripeProduct.ID, productPrice.ID, i.CreatedAt, i.UpdatedAt}
 
 	if err := i.db.Debug().Exec(insertQuery, params...).Error; err != nil {
 		log.Printf("error while saving to db: %v\n", err)
@@ -92,8 +94,49 @@ func (i *ShopItem) Create(data *ShopItemCreate) error {
 	return nil
 }
 
-func (i *ShopItem) Update() error {
+func (i *ShopItem) Update(data *ShopItemUpdate) error {
+	if data == nil {
+		return ErrShopItemUpdateBlank
+	}
+
+	if err := data.Validate(); err != nil {
+		return err
+	}
+
+	i.ItemName = data.ItemName
+	i.ItemPicture = data.ItemPicture
+	i.ItemPrice = data.ItemPrice
+	i.ItemSalePrice = data.ItemSalePrice
+	i.ItemDescription = data.ItemDescription
+	i.Shippable = data.Shippable
+	i.Quantity = data.Quantity
 	i.setUpdatedAt()
+
+	if _, err := data.updateStripeProduct(i.StripeProductApiID, i.ItemName, i.ItemDescription); err != nil {
+		log.Printf("error occurred while updating stripe product: %v", err)
+		return err
+	}
+
+	itemPrice := i.ItemPrice
+	if i.ItemSalePrice != nil {
+		itemPrice = *data.ItemSalePrice
+	}
+
+	if _, err := data.updateStripeProductPrice(i.StripePriceApiID, itemPrice); err != nil {
+		log.Printf("error occurred while updating stripe product price: %v", err)
+		return err
+	}
+
+	updateQuery := `UPDATE shop_items SET item_name = ?, item_picture = ?, item_price = ?, item_sale_price = ?, item_description = ?, 
+		shippable = ?, quantity = ?, updated_at = ? WHERE id = ? AND deleted_at IS NULL`
+
+	params := []interface{}{i.ItemName, i.ItemPicture, i.ItemPrice, i.ItemSalePrice, i.ItemDescription, i.Shippable,
+		i.Quantity, i.UpdatedAt, i.ID}
+
+	if err := i.db.Debug().Exec(updateQuery, params...).Error; err != nil {
+		log.Printf("error while updating shop item: %v\n", err)
+		return ErrInternal
+	}
 
 	return nil
 }
