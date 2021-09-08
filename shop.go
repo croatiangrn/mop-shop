a70bad1b4d3986e1db9e78b9eaa9f8fbdc384957
Marko Mikulic <mikulic.marko996@gmail.com>
2021-09-08 12:05:15 +0200

    Added user orders basic structs and methods
    Added empty ShopItem methods

--- shop.go
+++ shop.go
@@ -11,6 +11,7 @@ type ShopItem struct {
 	ItemPicture   *string    `gorm:"default:null;type:varchar(255);" json:"item_picture"`
 	ItemPrice     float32    `gorm:"not null;" json:"item_price"`
 	ItemSalePrice *float32   `gorm:"default: null;" json:"item_sale_price"`
+	Quantity      int        `gorm:"not null; default:0;" json:"quantity"`
 	CreatedAt     time.Time  `gorm:"not null;" json:"created_at"`
 	UpdatedAt     time.Time  `gorm:"not null;" json:"updated_at"`
 	DeletedAt     *time.Time `json:"-"`
@@ -19,4 +20,24 @@ type ShopItem struct {
 
 func (i *ShopItem) TableName() string {
 	return "shop_items"
+}
+
+func NewShopItem(db *gorm.DB) *ShopItem {
+	return &ShopItem{db: db}
+}
+
+func (i *ShopItem) Validate() error {
+	return nil
+}
+
+func (i *ShopItem) Create() error {
+	return nil
+}
+
+func (i *ShopItem) Update() error {
+	return nil
+}
+
+func (i *ShopItem) Delete() error {
+	return nil
 }
