4bdc2800906a4c3fab70c72341401a31c3cb3b37
Marko Mikulic <mikulic.marko996@gmail.com>
2021-09-08 12:36:46 +0200

    Added a few more columns to ShopItem struct

--- shop.go
+++ shop.go
@@ -6,16 +6,19 @@ import (
 )
 
 type ShopItem struct {
-	ID            int        `gorm:"primaryKey" json:"id"`
-	ItemName      string     `gorm:"not null;type:varchar(255);" json:"item_name"`
-	ItemPicture   *string    `gorm:"default:null;type:varchar(255);" json:"item_picture"`
-	ItemPrice     float32    `gorm:"not null;" json:"item_price"`
-	ItemSalePrice *float32   `gorm:"default: null;" json:"item_sale_price"`
-	Quantity      int        `gorm:"not null; default:0;" json:"quantity"`
-	CreatedAt     time.Time  `gorm:"not null;" json:"created_at"`
-	UpdatedAt     time.Time  `gorm:"not null;" json:"updated_at"`
-	DeletedAt     *time.Time `json:"-"`
-	db            *gorm.DB
+	ID                 int        `gorm:"primaryKey" json:"id"`
+	ItemName           string     `gorm:"not null;type:varchar(255);" json:"item_name"`
+	ItemPicture        *string    `gorm:"default:null;type:varchar(255);" json:"item_picture"`
+	ItemPrice          float32    `gorm:"not null;" json:"item_price"`
+	ItemSalePrice      *float32   `gorm:"default: null;" json:"item_sale_price"`
+	ItemDescription    *string    `gorm:"type:text;default:null;" json:"item_description"`
+	Shippable          bool       `gorm:"not null;default:false;" json:"shippable"`
+	Quantity           int        `gorm:"not null; default:0;" json:"quantity"`
+	StripeProductApiID *string    `gorm:"type:varchar(255)" json:"stripe_product_api_id"`
+	CreatedAt          time.Time  `gorm:"not null;" json:"created_at"`
+	UpdatedAt          time.Time  `gorm:"not null;" json:"updated_at"`
+	DeletedAt          *time.Time `json:"-"`
+	db                 *gorm.DB
 }
 
 func (i *ShopItem) TableName() string {
