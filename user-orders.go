a6832b25981e28c829d871f3baec25283ee8d464
Marko Mikulic <mikulic.marko996@gmail.com>
2021-09-10 00:10:44 +0200

    Updated UpdateEmptyOrderAfterCheckout method of UserOrder struct

--- user-orders.go
+++ user-orders.go
@@ -2,8 +2,10 @@ package mop_shop
 
 import (
 	"errors"
+	"github.com/stripe/stripe-go/v72/checkout/session"
 	"gorm.io/gorm"
 	"log"
+	"strings"
 	"time"
 )
 
@@ -11,17 +13,13 @@ type UserOrder struct {
 	ID                      int       `gorm:"primaryKey;" json:"id"`
 	UserID                  int       `gorm:"not null;index:ix_user_order_id;" json:"user_id"`
 	TotalPrice              float32   `gorm:"not null;" json:"total_price"`
-	StripeSessionID         *string   `gorm:"type:varchar(255);" json:"stripe_session_id"`
+	StripeSessionID         *string   `gorm:"type:varchar(255);index:ix_stripe_session_id;" json:"stripe_session_id"`
 	CreatedAt               time.Time `gorm:"not null;" json:"created_at"`
 	UpdatedAt               time.Time `json:"updated_at"`
 	StripeClientReferenceID string    `gorm:"type:varchar(36);" json:"stripe_client_reference_id"`
 	IsCompleted             bool      `gorm:"default:false;" json:"-"`
 	orderItems              map[int]ItemWithStripeInfo
 	db                      *gorm.DB
-}
-
-func (i *UserOrderItem) TableName() string {
-	return "user_order_items"
 }
 
 func (o *UserOrder) GetOrderItems() map[int]ItemWithStripeInfo {
@@ -83,7 +81,41 @@ func (o *UserOrder) CreateEmptyOrder(userID int, clientReferenceID string) error
 	return nil
 }
 
-func (o *UserOrder) UpdateEmptyOrderAfterCheckout(totalPrice float32, products map[int]ItemWithStripeInfo) error {
+func (o *UserOrder) getProductsFromOrderBySessionID(sessionID string) (map[string]ItemWithStripeInfo, error) {
+	i := session.ListLineItems(sessionID, nil)
+	var productStripeIDs []string
+
+	products := make(map[string]ItemWithStripeInfo)
+
+	for i.Next() {
+		li := i.LineItem()
+		productStripeIDs = append(productStripeIDs, li.Price.Product.ID)
+		products[li.Price.Product.ID] = ItemWithStripeInfo{
+			UniqueStripePriceLookupKey: li.Price.LookupKey,
+			StripeProductApiID:         li.Price.Product.ID,
+			Price:                      float32(li.Price.UnitAmount),
+			Quantity:                   int(li.Quantity),
+		}
+	}
+
+	var dbShopItems []ItemWithStripeInfo
+	query := `SELECT id AS item_id, unique_stripe_price_lookup_key, item_price, item_sale_price, stripe_product_api_id FROM shop_items WHERE stripe_product_api_id IN (?)`
+	if err := o.db.Debug().Raw(query, productStripeIDs).Scan(&dbShopItems).Error; err != nil {
+		log.Printf("error while getting shop items: %v\n", err)
+		return nil, ErrInternal
+	}
+
+	for j := range dbShopItems {
+		if obj, ok := products[dbShopItems[j].StripeProductApiID]; ok {
+			obj.ItemID = dbShopItems[j].ItemID
+			products[dbShopItems[j].StripeProductApiID] = obj
+		}
+	}
+
+	return products, nil
+}
+
+func (o *UserOrder) UpdateEmptyOrderAfterCheckout(sessionID string, totalPrice float32, products map[string]ItemWithStripeInfo) error {
 	if o.ID == 0 {
 		return ErrInvalidUserOrderID
 	}
@@ -97,9 +129,37 @@ func (o *UserOrder) UpdateEmptyOrderAfterCheckout(totalPrice float32, products m
 		}
 	}()
 
-	// TODO: Update updated_at, is_completed, total_price for UserOrder!
+	query := `UPDATE user_orders SET updated_at = ?, is_completed = ?, total_price = ? WHERE session_id = ?`
+	if err := tx.Debug().Exec(query, time.Now(), true, totalPrice, sessionID).Error; err != nil {
+		tx.Rollback()
+		log.Printf("error while updating user order: %v\n", err)
+		return ErrInternal
+	}
 
-	// TODO: Insert user_order_items here!
+	var orderItemsQuerySB strings.Builder
+	var orderItemsQueryParams []interface{}
+
+	orderItemsQuery := `INSERT INTO user_order_items (user_order_id, shop_item_id, item_price, quantity) VALUES `
+	orderItemsQuerySB.WriteString(orderItemsQuery)
+
+	lastAvailableIndex := len(products) - 1
+	counter := 0
+	for i := range products {
+		if counter == lastAvailableIndex {
+			orderItemsQuerySB.WriteString(`(?, ?, ?, ?) `)
+		} else {
+			orderItemsQuerySB.WriteString(`(?, ?, ?, ?), `)
+		}
+
+		orderItemsQueryParams = append(orderItemsQueryParams, o.ID, products[i].Price, products[i].ItemID, products[i].Quantity)
+		counter++
+	}
+
+	if err := tx.Debug().Exec(orderItemsQuerySB.String(), orderItemsQueryParams...).Error; err != nil {
+		tx.Rollback()
+		log.Printf("error while inserting user order items: %v\n", err)
+		return ErrInternal
+	}
 
 	if err := tx.Commit().Error; err != nil {
 		log.Printf("error while comitting transaction in userOrder.UpdateEmptyOrderAfterCheckout: %v\n", err)
@@ -180,6 +240,11 @@ func (o *UserOrder) GetOrderByID(orderID int) error {
 type UserOrderItem struct {
 	ID          int     `gorm:"primaryKey;" json:"id"`
 	UserOrderID int     `gorm:"not null;index:ix_user_order_item_order_id;" json:"user_order_id"`
+	ShopItemID  int     `gorm:"not null;" json:"shop_item_id"`
 	ItemPrice   float32 `gorm:"not null;" json:"item_price"`
 	Quantity    int     `gorm:"not null;"`
+}
+
+func (i *UserOrderItem) TableName() string {
+	return "user_order_items"
 }
